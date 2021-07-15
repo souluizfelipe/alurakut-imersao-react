73e79cd191bd9ef419d667a84706ff31ed10b42c
Luiz Felipe Pereira <luiz.strina@gmail.com>
2021-07-15 08:52:20 -0300

    create profileSidebar component file and inport to index.js on pages

---
+++ src/components/ProfileSidebar/index.js
@@ -0,0 +1,22 @@
+import { AlurakutProfileSidebarMenuDefault } from '../../lib/AlurakutCommons'
+import Box from '../Box'
+
+function ProfileSidebar(props) {
+  return (
+    <Box as="aside" style={{padding: '12px'}}>
+      <img src={`https://github.com/${props.githubUser}.png`} style={{ borderRadius: '8px' }} />
+      <hr />
+
+      <p>
+        <a className="boxLink" href={`https://github.com/${props.githubUser}`}>
+          @{props.githubUser}
+        </a>
+      </p>
+      <hr />
+
+      <AlurakutProfileSidebarMenuDefault />
+    </Box>
+  )
+}
+
+export default ProfileSidebar
